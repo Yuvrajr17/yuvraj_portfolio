148281195ba5e27f2581c25175b4512d1e500d31
yuvraj <yuvrajsinghrajpoot521@gmail.com>
2026-03-21 23:38:51 +0530

    Fix CORS for production

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true
+  origin: '*',
+  credentials: false
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
